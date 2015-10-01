d01c25659d16ec939494edff8e45d31e75306318
csabagaspar <gaspar.csaba@gmail.com>
2015-10-01 13:08:41 +0000

    added counthi kata

--- src/test/codingbatSpec.js
+++ src/test/codingbatSpec.js
@@ -10,17 +10,38 @@ describe("string2", function(){
        expect("aa").toEqual(doubleChar("a"));
        expect("bb").toEqual(doubleChar("b"));
        expect("aaaa").toEqual(doubleChar("aa"));
-
         /*
         1. {}->null, null->constant
         2. unconditional->if , {}->null, null->constant
         3. constant->variable, add computing
         4. if to while
         */
-
        expect("bbbb").toEqual(doubleChar("bb"));
        expect("aabb").toEqual(doubleChar("ab"));
        expect("aaaabbbb").toEqual(doubleChar("aabb"));
        expect("aabbcc").toEqual(doubleChar("abc"));
+    });
+    var countHi = function(str){
+        if(str === undefined || str === "")
+            return 0;
+
+        var count = 0;
+        for(var index = 0; str.length > index;  index++)
+            if(str.charAt(index) === 'h' && str.charAt(index + 1) === 'i')
+                count++;
+        return count;
+    };
+
+    it("countHiTest", function(){
+        expect(0).toEqual(countHi());
+        expect(0).toEqual(countHi(undefined));
+        expect(0).toEqual(countHi(""));
+
+        expect(0).toEqual(countHi("valami"));
+
+        expect(1).toEqual(countHi("hi"));
+        expect(1).toEqual(countHi("hia"));
+        expect(1).toEqual(countHi("ahi"));
+        expect(2).toEqual(countHi("hihi"));
     });
 });
